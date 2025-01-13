004e5e68ee27ce63d6b9ccfc69d66bba06a7c418
Prado Ricardo De Leon Nolasco <Prado.code@outlook.com>
2025-01-13 12:55:48 -0400

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : "one" ,component: FirstComponent},
+    {path : "two" ,component: SecondComponent}
+];
